a9b97b64d2942dd99766b47cea995f96cd6d1463
James Edington <codegeek98@gmail.com>
2017-11-25 00:31:53 +0000

    Refactor code, change syntax
    Now file can be specified

--- register.sh
+++ register.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+# USAGE: ./register.sh ../../tls/example.com.pem example.com [www.example.com] [blog.example.com] [...]
+file="${1%.pem}"
+tld="${2}"
+shift 2
 SAN=("${@}")
-SAN=( "${SAN[@]/#/DNS:}" )
 openssl req -new\
  -outform pem\
- -out "${1}.csr"\
+ -out "${file}.csr"\
  -newkey ec\
   -pkeyopt ec_paramgen_curve:secp384r1\
   -nodes\
-  -keyout "${1}.key"\
- -subj /CN="${1}"\
+  -keyout "${file}.key"\
+ -subj /CN="${tld}"\
  -reqexts SAN\
  -config <(
   (
-   printf "[SAN]\nsubjectAltName=";(IFS=","; printf '%s\n' "${SAN[*]}")
+   printf "[SAN]\nsubjectAltName=";(IFS=","; printf '%s\n' "${SAN[*]/#/DNS:}")
   )| cat /etc/ssl/openssl.cnf -
  )\
  -keyform pem &&\
-openssl req -in "${1}.csr" -text -noout &&\
-python acme_tiny.py\
+openssl req -in "${file}.csr" -text -noout &&\
+python acme-tiny/acme_tiny.py\
  --account-key /etc/ssh/ssh_host_rsa_key\
- --acme-dir "/var/www/${1}/.well-known/acme-challenge/"\
- --csr "${1}.csr"\
- > "${1}.crt" &&\
-openssl x509 -in "${1}.crt" -text -noout &&\
-cat "${1}.key" "${1}.crt" > "${1}.pem"
+ --acme-dir "/var/www/${tld}/.well-known/acme-challenge/"\
+ --csr "${file}.csr"\
+ > "${file}.crt" &&\
+openssl x509 -in "${file}.crt" -text -noout &&\
+cat "${file}.key" "${file}.crt" > "${file}.pem"
